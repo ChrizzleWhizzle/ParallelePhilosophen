b11f650ea7e186f03490ec13bf52e7f9ed782116
Christian Abele <cabele@hm.edu>
2015-10-28 16:48:09 +0100

    added some functionality to philosoph

--- src/Philosoph.java
+++ src/Philosoph.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Philosoph {
+public class Philosoph extends Thread {
     // Process:
     // start with meditate
     // if hungry go to dining room
@@ -15,5 +15,67 @@ public class Philosoph {
     // horst
 
     private ReentrantLock _lock = new ReentrantLock(true);
+    private final int eatTime = 1000;
+    private final int meditateTime = 1000;
+    private final int sleepTime = 10000;
+    private static int event=0;
+
+    private int id;
+    private Seat seat;
+    private static final int maxMealsEaten = 3;
+    private int mealsEaten;
+
+    public Philosoph(int id) {
+        this.id = id;
+    }
+
+    public void run() {
+        while (true) {
+
+            if (!takeSeat()) continue;
+            // take right fork
+            // take left fork
+            eat();
+            if (mealsEaten == maxMealsEaten) goToSleep();
+            else meditate();
+        }
+    }
+
+    private void eat() {
+        try {
+            postMsg("eating for " + eatTime);
+            mealsEaten++;
+            Thread.sleep(eatTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void goToSleep() {
+        try {
+            postMsg("sleeping for " + sleepTime);
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void meditate() {
+        try {
+            postMsg("meditating for " + meditateTime);
+            Thread.sleep(meditateTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void postMsg(String str) {
+        System.out.printf("%d %d Philosopher %d %s\n",
+                System.currentTimeMillis(), ++event, id, str);
+    }
+
+    private boolean takeSeat() {
+        return false;
+    }
 
 }
