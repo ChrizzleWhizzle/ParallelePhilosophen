b7fd8a29d25685ce26e211e1b4bde434ed1cb534
Christian Abele <cabele@hm.edu>
2015-10-28 16:29:20 +0100

    added Classes

---
+++ src/Fork.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Chris on 28.10.2015.
+ */
+public class Fork {
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Chris on 28.10.2015.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("started");
+    }
+}
